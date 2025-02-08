ac8f34fc531cb35b4e4c534f84d368f6160fe043
Paweł Grzesiewicz <p.grzesiewicz@gmail.com>
2025-02-09 00:46:36 +0100

    logic for close nav offcanvas after clicking a link

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -16,6 +16,7 @@ export default function NavBar({ locale }: NavBarProps) {
   const t = useTranslations("NavbarLinks");
 
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -32,40 +33,73 @@ export default function NavBar({ locale }: NavBarProps) {
   }, []);
 
 
+  const handleNavClose = () => {
+    setIsNavOpen(false);
+  };
+
   return (
     <Navbar expand="lg" fixed="top" className={`mb-3 ${isScrolled ? 'scroll-bg' : ''}`}>
       <Container fluid="xxl">
         <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-          <Logo/>
+          <Logo />
         </Offcanvas.Title>
 
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-lg`} />
+        <Navbar.Toggle
+          aria-controls={`offcanvasNavbar-expand-lg`}
+          onClick={() => setIsNavOpen(!isNavOpen)}
+        />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-lg`}
           aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
           placement="end"
+          show={isNavOpen}
+          onHide={() => setIsNavOpen(false)}
         >
           <Offcanvas.Header closeButton>
             <Offcanvas.Title id={`offcanvasNavbarLabel-expand-lg`}>
-              <Logo/>
+              <Logo />
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3 nav-collaps">
-              <Link href={`/${locale}/`} className="nav-link" aria-label="Go to Home">
+              <Link
+                href={`/${locale}/`}
+                className="nav-link"
+                aria-label="Go to Home"
+                onClick={handleNavClose}
+              >
                 {t('home')}
               </Link>
               <Link
-                href={`/${locale}/about`} className="nav-link" aria-label="Go to About">
+                href={`/${locale}/about`}
+                className="nav-link"
+                aria-label="Go to About"
+                onClick={handleNavClose}
+              >
                 {t("about")}
               </Link>
-              <Link href={`/${locale}/projects`} className="nav-link" aria-label="Go to Projects">
+              <Link
+                href={`/${locale}/projects`}
+                className="nav-link"
+                aria-label="Go to Projects"
+                onClick={handleNavClose}
+              >
                 {t("projects")}
               </Link>
-              <Link href={`/${locale}/contact`} className="nav-link" aria-label="Go to Contact">
+              <Link
+                href={`/${locale}/contact`}
+                className="nav-link"
+                aria-label="Go to Contact"
+                onClick={handleNavClose}
+              >
                 {t("contact")}
               </Link>
-              <Link href={`/${locale}/blog`} className="nav-link disabled" aria-label="Blog (currently unavailable)">
+              <Link
+                href={`/${locale}/blog`}
+                className="nav-link disabled"
+                aria-label="Blog (currently unavailable)"
+                onClick={handleNavClose}
+              >
                 {t("blog")}
               </Link>
             </Nav>
